0251190f8bd6060d8915d4e726590e0cc02ed0ff
as1590-dot <as1590@srmist.edu.in>
2026-03-17 09:21:17 +0530

    modified uc1

--- HelloApp.java
+++ HelloApp.java
@@ -1,6 +1,5 @@
 public class HelloApp {
 	public static void main(String[] args) {
-		String name[] = new args[1];
-	    System.out.println("Hello," + name + "!");
+	    System.out.println("Hello, World");
 	}
 }
